3f371bc66e4be53019d6be2d9390baff26e98128
Adam-dunno <goshaivanov2019@mail.ru>
2024-05-20 06:17:57 +0300

    build

---
+++ rk2/_build/CMakeFiles/BuilderExample.dir/Program.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/BuilderExample.dir/Program.cpp.o: /home/adam/rk2/Program.cpp \
+ /usr/include/stdc-predef.h
---
+++ rk2/_build/CMakeFiles/BuilderExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BuilderExample"
+  "BuilderExample.pdb"
+  "CMakeFiles/BuilderExample.dir/Program.cpp.o"
+  "CMakeFiles/BuilderExample.dir/Program.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BuilderExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
